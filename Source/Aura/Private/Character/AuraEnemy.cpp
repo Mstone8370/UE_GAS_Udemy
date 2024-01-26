1ba45c7e2f5cd6d30900f029f93f5aedf4903b54
mstone8370 <mstone8370@mgmail.com>
2024-01-26 21:14:42 +0900

    Post Process Highlight

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,42 @@
 
 #include "Character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+    if (IsValid(GetMesh()))
+    {
+        GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+    }
+    if (IsValid(Weapon))
+    {
+        Weapon->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+    }
+}
+
 void AAuraEnemy::HighlightActor()
 {
-    bHighlighted = true;
+    if (IsValid(GetMesh()))
+    {
+        GetMesh()->SetRenderCustomDepth(true);
+        GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+    }
+    if (IsValid(Weapon))
+    {
+        Weapon->SetRenderCustomDepth(true);
+        Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+    }
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-    bHighlighted = false;
+    if (IsValid(GetMesh()))
+    {
+        GetMesh()->SetRenderCustomDepth(false);
+    }
+    if (IsValid(Weapon))
+    {
+        Weapon->SetRenderCustomDepth(false);
+    }
 }
