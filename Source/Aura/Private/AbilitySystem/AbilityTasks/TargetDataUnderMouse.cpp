7f1274b72713da4bccca64817d7a1ea88c09ca8a
mstone8370 <mstone8370@gmail.com>
2024-02-11 14:35:34 +0900

    Send Mouse Cursor Data

--- Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -3,30 +3,52 @@
 
 #include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
 
-#include "Character/AuraCharacter.h"
-#include "Player/AuraPlayerController.h"
+#include "AbilitySystemComponent.h"
 
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
     UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
-    // MyObj->Ability = OwningAbility;
     return MyObj;
 }
 
 void UTargetDataUnderMouse::Activate()
 {
-    if (!Ability)
+    if (Ability->IsLocallyControlled())
     {
-        return;
+        SendMouseCursorData();
     }
+    else
+    {
+        // TODO: We are on the server, to listen for target data;
+    }
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+    // 이 scope에 있는 것들은 prediction되어야한다는 의미
+    FScopedPredictionWindow ScopedPrediction(AbilitySystemComponent.Get());
     
+    FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
+
     if (const APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get())
     {
         FHitResult CursorHit;
-        if (PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit))
-        {
-            const FVector Location = CursorHit.ImpactPoint;
-            ValidData.Broadcast(Location);
-        }
+        PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+        Data->HitResult = CursorHit;
+    }
+
+    FGameplayAbilityTargetDataHandle DataHandle;
+    DataHandle.Add(Data);
+    AbilitySystemComponent->ServerSetReplicatedTargetData(
+        GetAbilitySpecHandle(),
+        GetActivationPredictionKey(),
+        DataHandle,
+        FGameplayTag(),
+        AbilitySystemComponent->ScopedPredictionKey
+    );
+
+    if (ShouldBroadcastAbilityTaskDelegates())
+    {
+        ValidData.Broadcast(DataHandle);
     }
 }
