e672c94e216d7f5c34d9f4a7cded597c37fb2e37
mstone8370 <mstone8370@gmail.com>
2024-02-19 12:07:15 +0900

    Blackboard Keys

--- Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -4,6 +4,8 @@
 #include "AI/BTService_FindNearestPlayer.h"
 
 #include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
@@ -11,4 +13,15 @@ void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, u
 
     GEngine->AddOnScreenDebugMessage(1, 3.f, FColor::Red, *OwnerComp.GetAIOwner()->GetName());
     GEngine->AddOnScreenDebugMessage(2, 3.f, FColor::Green, *OwnerComp.GetOwner()->GetName());
+
+    OwnerComp.GetBlackboardComponent()->GetValueAsObject(BlackboardKey.SelectedKeyName);
+
+    const APawn* OwningPawn = OwnerComp.GetAIOwner()->GetPawn();
+
+    const FName TargetTag = OwningPawn->ActorHasTag(FName("Player")) ? FName("Enemy") : FName("Player");
+
+    TArray<AActor*> ActorsWithTag;
+    UGameplayStatics::GetAllActorsWithTag(OwningPawn, TargetTag, ActorsWithTag);
+
+    
 }
--- Source/Aura/Public/AI/BTService_FindNearestPlayer.h
+++ Source/Aura/Public/AI/BTService_FindNearestPlayer.h
@@ -16,4 +16,10 @@ class AURA_API UBTService_FindNearestPlayer : public UBTService_BlackboardBase
 
 protected:
     virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+    UPROPERTY(BlueprintReadOnly, EditAnywhere)
+    FBlackboardKeySelector TargetToFollowSelector;
+
+    UPROPERTY(BlueprintReadOnly, EditAnywhere)
+    FBlackboardKeySelector DistanceToTargetSelector;
 };
