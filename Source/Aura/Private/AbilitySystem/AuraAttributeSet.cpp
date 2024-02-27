dc014e2d745a6d843444bfbec8015c6f2da70226
mstone8370 <mstone8370@gmail.com>
2024-02-27 14:41:22 +0900

    Leveling Up

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -148,10 +148,32 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
         SetIncomingXP(0.f);
 
         // TODO: See if we should level up
-        if (Props.SourceCharacter->Implements<UPlayerInterface>())
-        {
-            IPlayerInterface::Execute_AddToXP(Props.SourceCharacter, LocalIncomingXP);
 
+        // Source Character is the owner, since GA_ListenForEvents applies GE_EventBasedEffect, adding to IncomingXP
+        if (Props.SourceCharacter->Implements<UPlayerInterface>() && Props.SourceCharacter->Implements<UCombatInterface>())
+        {
+            const int32 CurrentLevel = ICombatInterface::Execute_GetPlayerLevel(Props.SourceCharacter);
+            const int32 CurrentXP = IPlayerInterface::Execute_GetXP(Props.SourceCharacter);
+
+            const int32 NewLevel = IPlayerInterface::Execute_FindLevelForXP(Props.SourceCharacter, CurrentXP + LocalIncomingXP);
+            const int32 NumOfLevelUps = NewLevel - CurrentLevel;
+            if (NumOfLevelUps > 0)
+            {
+                // TODO:
+                int32 AttributePointsReward = IPlayerInterface::Execute_GetAttributePointsReward(Props.SourceCharacter, CurrentLevel);
+                int32 SpellPointsReward = IPlayerInterface::Execute_GetSpellPointsReward(Props.SourceCharacter, CurrentLevel);
+                
+                IPlayerInterface::Execute_AddToPlayerLevel(Props.SourceCharacter, NumOfLevelUps);
+                IPlayerInterface::Execute_AddToAttributePoints(Props.SourceCharacter, AttributePointsReward);
+                IPlayerInterface::Execute_AddToSpellPoints(Props.SourceCharacter, SpellPointsReward);
+                
+                SetHealth(GetMaxHealth());
+                SetMana(GetMaxMana());
+
+                IPlayerInterface::Execute_LevelUp(Props.SourceCharacter);
+            }
+
+            IPlayerInterface::Execute_AddToXP(Props.SourceCharacter, LocalIncomingXP);
         }
     }
 }
--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -16,20 +16,27 @@ class AURA_API AAuraCharacter : public AAuraCharacterBase, public IPlayerInterfa
 	GENERATED_BODY()
 
 public:
-    AAuraCharacter();
+	AAuraCharacter();
 
-    virtual void PossessedBy(AController* NewController) override;
-    virtual void OnRep_PlayerState() override;
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
 
-    //~ Begin Combat Interface
-    virtual int32 GetPlayerLevel_Implementation() override;
-    //~ End Combat Interface
+	//~ Begin Combat Interface
+	virtual int32 GetPlayerLevel_Implementation() override;
+	//~ End Combat Interface
 
-    //~ Begine Player Interface
-    virtual void AddToXP_Implementation(int32 InXP) override;
-    virtual void LevelUp_Implementation() override;
-    //~ End Player Interface
+	//~ Begine Player Interface
+	virtual int32 FindLevelForXP_Implementation(int32 InXP) const;
+	virtual int32 GetXP_Implementation() const;
+	virtual int32 GetAttributePointsReward_Implementation(int32 Level) const;
+	virtual int32 GetSpellPointsReward_Implementation(int32 Level) const;
+	virtual void AddToXP_Implementation(int32 InXP) override;
+	virtual void AddToPlayerLevel_Implementation(int32 InPlayerLevel) override;
+	virtual void AddToAttributePoints_Implementation(int32 InAttribuetPorints) override;
+	virtual void AddToSpellPoints_Implementation(int32 InSpellPoints) override;
+	virtual void LevelUp_Implementation() override;
+	//~ End Player Interface
 
 private:
-    virtual void InitAbilityActorInfo() override;
+	virtual void InitAbilityActorInfo() override;
 };
