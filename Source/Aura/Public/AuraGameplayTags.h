db508bcab81923654b4a0b07e77e2959b9f5080d
mstone8370 <mstone8370@gmail.com>
2024-02-29 18:05:20 +0900

    Ability Status and Type

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -70,6 +70,18 @@ public:
     FGameplayTag Abilities_Summon;
     FGameplayTag Abilities_Fire_FireBolt;
 
+    FGameplayTag Abilities_HitReact;
+
+    FGameplayTag Abilities_Status_Locked;
+    FGameplayTag Abilities_Status_Eligible;
+    FGameplayTag Abilities_Status_Unlocked;
+    FGameplayTag Abilities_Status_Equipped;
+
+    FGameplayTag Abilities_Type_Offensive;
+    FGameplayTag Abilities_Type_Passive;
+    FGameplayTag Abilities_Type_None;
+
+
     // Cooldowns
     FGameplayTag Cooldown_Fire_FireBolt;
 
